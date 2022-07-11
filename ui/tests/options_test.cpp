5395ad714e750cc074913045f0c98f5fb03b0fad
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2022-07-11 15:44:38 +0300

    added options_test refactored options_test

--- ui/tests/options_test.cpp
+++ ui/tests/options_test.cpp
@@ -2,16 +2,28 @@
 
 #include <gtest/gtest.h>
 
+class options_test : public ui::options
+{
+  public:
+    options_test() = default;
+};
+
 TEST(set_and_get_language, russian)
 {
-    auto &test = ui::options::get_options();
-    test.set_ru_language();
-    ASSERT_EQ(ui::language_id::russian, test.get_language());
+    options_test test;
+    test.set_language(ui::options::language_id::russian);
+    ASSERT_EQ(ui::options::language_id::russian, test.get_language());
 }
 
 TEST(set_and_get_language, english)
 {
-    auto &test = ui::options::get_options();
-    test.set_en_language();
-    ASSERT_EQ(ui::language_id::english, test.get_language());
+    options_test test;
+    test.set_language(ui::options::language_id::english);
+    ASSERT_EQ(ui::options::language_id::english, test.get_language());
+}
+
+TEST(set_and_get_language, default_language_should_be_english)
+{
+    options_test test;
+    ASSERT_EQ(ui::options::language_id::english, test.get_language());
 }
